7e24d786537bde46c9d237e75eb1898bc047aae6
Ridho Aulia Rahman <144804261+EngRidhoNet@users.noreply.github.com>
2025-02-27 11:27:07 +0700

    request

---
+++ app/Http/Requests/Transaction/Store.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Requests\Transaction;
+
+use Dotenv\Exception\ValidationException;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Http\Exceptions\HttpResponseException;
+// use Illuminate\Support\Facades\Validator;
+
+class Store extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return auth()->role === 'customer';
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'listing_id' => 'required|exists:listings,id',
+            'start_date' => 'required|date',
+            'end_date' => 'required|date|after_or_equal:start_date',
+        ];
+    }
+
+    protected function failedValidation(Validator $validator){
+        $errors = (new ValidationException( $validator))->errors();
+        throw new HttpResponseException(
+            response()->json()
+        )
+    }
+}
